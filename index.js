87070787a5f63943bb52bf3e93b3caec65a7be6a
targaro91 <antoniodpereracalas@gmail.com>
2023-06-04 20:41:44 -0400

    a

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const fs = require('fs');
 
 function plainToCSV(file) {
     var array = fs.readFileSync(file).toString().split("\n");
-    var csv = '';
+    var csv = 'Name,Given Name,Additional Name,Family Name,Yomi Name,Given Name Yomi,Additional Name Yomi,Family Name Yomi,Name Prefix,Name Suffix,Initials,Nickname,Short Name,Maiden Name,Birthday,Gender,Location,Billing Information,Directory Server,Mileage,Occupation,Hobby,Sensitivity,Priority,Subject,Notes,Language,Photo,Group Membership,Phone 1 - Type,Phone 1 - Value,Organization 1 - Type,Organization 1 - Name,Organization 1 - Yomi Name,Organization 1 - Title,Organization 1 - Department,Organization 1 - Symbol,Organization 1 - Location,Organization 1 - Job Description\n';
 
     let result = parseNameAndCel(array, 0)
     const line = lineCSV(result)
